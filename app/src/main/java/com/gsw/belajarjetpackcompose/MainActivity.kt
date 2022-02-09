30aee276e6373e357ed766eff724c13264bc7270
Ganang Samudra Wibawa <ganangsamudra1@gmail.com>
2022-02-09 17:06:59 +0700

    text

--- app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
@@ -3,11 +3,21 @@ package com.gsw.belajarjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gsw.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,5 +36,47 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Text(text = "Hello World")
+    Column {
+//        simple text
+        Text(
+            text = "Hello",
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+//        text panjang
+        val text = "Ini adalah contoh text panjang menggunakan jetpack compose"
+        Text(
+            text = text,
+            color = Color.DarkGray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+//        text underline
+        Text(
+            text = "Hello",
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textDecoration = TextDecoration.Underline
+        )
+//        text panjang maxline 1
+        Text(
+            text = text,
+            color = Color.DarkGray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            maxLines = 1
+        )
+//        text panjang dengan akhiran titik-titik
+        Text(
+            text = text,
+            color = Color.DarkGray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
