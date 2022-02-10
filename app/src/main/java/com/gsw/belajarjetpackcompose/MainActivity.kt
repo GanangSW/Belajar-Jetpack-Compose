4a694541265aed538fb14d863112514addb7f2bd
Ganang Samudra Wibawa <ganangsamudra1@gmail.com>
2022-02-10 18:22:35 +0700

    image

--- app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
@@ -3,17 +3,18 @@ package com.gsw.belajarjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.gsw.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,54 +36,26 @@ fun Greeting() {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
     ) {
-//        simple button
-        Button(onClick = { }) {
-            Text(text = "Simple Button")
-        }
 
-        Spacer(modifier = Modifier.height(5.dp))
-//        simple button custom background color
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.Yellow,
-                contentColor = Color.White
-            )
-        ) {
-            Text(text = "Simple Button")
-        }
+//        image local
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Image"
+        )
 
-        Spacer(modifier = Modifier.height(5.dp))
-//        text Button
-        TextButton(onClick = { /*TODO*/ }) {
-            Text(text = "Text Button")
-        }
-
-        Spacer(modifier = Modifier.height(5.dp))
-//        rounded button
-        Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(15.dp)) {
-            Text(text = "Rounded Button")
-        }
-
-        Spacer(modifier = Modifier.height(5.dp))
-//        outline button
-        OutlinedButton(
-            onClick = { /*TODO*/ },
-            border = BorderStroke(width = 2.dp, color = Color.Yellow)
-        ) {
-            Text(text = "Outline Button")
-        }
-        Spacer(modifier = Modifier.height(5.dp))
-//        outline button with icon
-        OutlinedButton(
-            onClick = { /*TODO*/ },
-            border = BorderStroke(width = 2.dp, color = Color.Yellow)
-        ) {
-            Icon(Icons.Default.Favorite, contentDescription = "Button")
-            Text(text = "Outline Icon Button")
-        }
+//        image internet
+        var urlImage = "https://via.placeholder.com/150"
+        Image(
+            painter = rememberImagePainter(data = urlImage),
+            contentDescription = "Image internet",
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp)
+        )
     }
 }
 
