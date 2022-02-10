4b9386906def8fcccfdca9f9bbf7af8e236ead33
Ganang Samudra Wibawa <ganangsamudra1@gmail.com>
2022-02-10 18:34:50 +0700

    box

--- app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -13,8 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.gsw.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,24 +39,20 @@ fun Greeting() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue)
     ) {
 
-//        image local
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Image"
-        )
+//        dengan box
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "background"
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "foreground"
+            )
+        }
 
-//        image internet
-        var urlImage = "https://via.placeholder.com/150"
-        Image(
-            painter = rememberImagePainter(data = urlImage),
-            contentDescription = "Image internet",
-            modifier = Modifier
-                .height(100.dp)
-                .width(100.dp)
-        )
     }
 }
 
