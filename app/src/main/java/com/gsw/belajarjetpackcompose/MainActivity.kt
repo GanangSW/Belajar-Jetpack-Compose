570b8b3a1f6f2e10ed85f2a6d7723bf4b232e6d6
Ganang Samudra Wibawa <ganangsamudra1@gmail.com>
2022-02-13 17:52:54 +0700

    snackbar

--- app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/gsw/belajarjetpackcompose/MainActivity.kt
@@ -1,27 +1,21 @@
 package com.gsw.belajarjetpackcompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
 import com.gsw.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,13 +28,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@ExperimentalMaterialApi
 @Composable
 fun Greeting() {
+
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             TopAppBar(
-                title = { Text(text = "Badge Box") },
+                title = { Text(text = "Snackbar") },
             )
         },
         content = {
@@ -49,18 +47,21 @@ fun Greeting() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxSize()
             ) {
-                BadgeBox(
-                    badgeContent = {
-                        Text(text = "15")
-                    },
-                    backgroundColor = Color.Blue,
-                    contentColor = Color.White
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Notifications, contentDescription = "",
-                        tint = MaterialTheme.colors.primary
-                    )
+
+                Button(onClick = {
+
+                    coroutineScope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            message = "Ini adalah snackbar",
+                            duration = SnackbarDuration.Short,
+                            actionLabel = "Cancel"
+                        )
+                    }
+
+                }) {
+                    Text(text = "Open Snackbar")
                 }
+
             }
         }
     )
